b6575300176f703e3526cd4d7c641bea94f7bcb7
hikarical <Hikariion@foxmail.com>
2024-02-27 20:21:04 +0800

    add satellite.json to local

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
     const fetchNodeNames = useClusterDataStore(state => state.fetchNodeNames);
     const fetchClusterData = useClusterDataStore(state => state.fetch);
-    const CLUSTER_DATA_URL = "http://localhost:9091/server/satellite.json";
+    const CLUSTER_DATA_URL = "satellite.json";
     useEffect(() => {
       fetchClusterData(CLUSTER_DATA_URL);
         
